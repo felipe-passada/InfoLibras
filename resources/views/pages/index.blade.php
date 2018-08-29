f2e4b1db5256cd1a51bb59e7af02b80190a037c1
felipepassada <felipepassada@gmail.com>
2018-08-29 16:51:37 -0300

    criação de uma rota de testes

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body id="page-index">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">IFSP - Info Libras</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active ml-2">
+                    <a class="nav-link" href="#"><i class="material-icons">accessibility</i> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active ml-2">
+                    <a class="nav-link" href="#"><i class="material-icons">info_outline</i> Informações</a>
+                </li>
+                
+                <li class="nav-item active ml-2">
+                    <a class="nav-link disabled" href="#"><i class="material-icons">map</i> Mapa</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar <i class="material-icons">search</i></button>
+            </form>
+        </div>
+    </nav>
+
+    <div class="container mt-2">
+        <div class="row mt-2">
+            <div class="col mt-2">
+
+            </div>
+        </div>
+    </div>
+    
+    <!-- Footer -->
+
+    <script src="/js/app.js"></script>
+</body>
+</html>
