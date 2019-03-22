c441a180aa6224932c12089f0323636e97fef5bc
Cesar Cutolo <cutolocesar@gmail.com>
2019-03-22 20:22:14 -0300

    Atualzação Layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+Auth::routes();
+
 Route::get('/', function(){
     return view('welcome');
 });
 
+Route::get('/home', 'HomeController@index')->middleware('auth')->name('home');
 
+Route::get('/contents/home', function() {
+    return view("contents/home");
+});
