8b32bfad410f8d278798a51df6bcb189fbec5fcd
felipe <felipepassada@gmail.com>
2019-08-19 15:29:47 -0300

    fixed assests

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,8 +16,8 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <link href="{{secure_asset('css/app.css')}}" rel="stylesheet">
-  <link href="{{secure_asset('js/app.js')}}" rel="stylesheet">
+  <link href="/css/app.css" rel="stylesheet">
+  <link href="/js/app.js" rel="stylesheet">
 
 </head>
 
